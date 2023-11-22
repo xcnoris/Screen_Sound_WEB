f210fc949f92c2e0a6bb23176d99054020eb5c83
bessax <andrebessax@gmail.com>
2023-11-22 11:39:32 -0300

    #Vídeo 2.3 - Injeção de dependência

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,34 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
+using System.Data.SqlTypes;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/Artistas", () =>
-{
-    var dal = new DAL<Artista>(new ScreenSoundContext());
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
+{  
     return Results.Ok(dal.Listar());
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal,string nome) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     var artista = dal.RecuperarPor(a=>a.Nome.ToUpper().Equals(nome.ToUpper()));
     if (artista is null)
     {
         return Results.NotFound();
     }
     return Results.Ok(artista);
-   
+    
 });
 
-app.MapPost("/Artistas", ([FromBody] Artista artista) =>
-{
-    var dal = new DAL<Artista>(new ScreenSoundContext());
+app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal,[FromBody] Artista artista) =>
+{   
     dal.Adicionar(artista);
     return Results.Ok();
 });
