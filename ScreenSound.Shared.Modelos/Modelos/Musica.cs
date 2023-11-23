cab9cd1c8bbc6597c96811334258c0bee70beb94
bessax <andrebessax@gmail.com>
2023-11-23 14:37:45 -0300

    #Mão na massa implementação dos endpoints de música.

--- ScreenSound.Shared.Modelos/Modelos/Musica.cs
+++ ScreenSound.Shared.Modelos/Modelos/Musica.cs
@@ -2,6 +2,10 @@
 
 public class Musica
 {
+    public Musica()
+    {
+        
+    }
     public Musica(string nome)
     {
         Nome = nome;
@@ -10,6 +14,7 @@ public class Musica
     public string Nome { get; set; }
     public int Id { get; set; }
     public int? AnoLancamento { get; set; }
+    public int? ArtistaId { get; set; }
     public virtual Artista? Artista { get; set; }
 
     public void ExibirFichaTecnica()
